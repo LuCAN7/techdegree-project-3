c29aa7f652441d96fbd6d622e098bcc9d51d511d
Lucan F <dapps911@hotmail.com>
2019-06-27 00:25:13 -0400

    Added job role input uninstrusively

--- js/script.js
+++ js/script.js
@@ -1,6 +1,23 @@
 
-$( document ).ready(function() {
-    // Handler for .ready() called.
-    console.log('DOING GOOD!!!');
+$( document ).ready(function(e) {
     $('input#name').focus();
+    // e.preventDefault();
   });
+  
+  
+const jobRole = document.getElementById('other-title');
+const select = document.querySelector('#title');
+
+jobRole.style.display = 'none';
+
+select.addEventListener('change', function(){
+
+  if (select.selectedIndex === 5 ) {
+    // A more dynamic way...incase more options are added
+    // console.log(select.options[select.selectedIndex].text);
+    jobRole.style.display = 'block';
+  } else { 
+    jobRole.style.display = 'none';
+  }
+  
+});
